ea1a991d3f68fb9aaaa91acbbc2b6f771ada6768
fathur rizky <61815491+rzkyfathur@users.noreply.github.com>
2022-08-29 13:32:48 +0700

    nih sat

--- Inventory/produksi/produksi.php
+++ Inventory/produksi/produksi.php
@@ -11,6 +11,7 @@
 
     <title>SB Admin 2 - Blank</title>
 
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.10/css/select2.min.css" rel="stylesheet" />
     <!-- Custom fonts for this template-->
     <link href="../../assets/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
@@ -20,6 +21,7 @@
 
     <!-- Custom styles for this page -->
     <link href="../../assets/vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
+
 
 </head>
 
@@ -241,7 +243,7 @@
 
 
         <!-- Modal -->
-        <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal fade" id="exampleModal" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
             <div class="modal-dialog" role="document">
                 <div class="modal-content">
                     <div class="modal-header">
@@ -253,8 +255,13 @@
                     <div class="modal-body">
                         <form action="#">
                             <div class="form-group">
-                                <label for="recipient-name" class="col-form-label">Nama</label>
-                                <input type="text" class="form-control" id="recipient-name">
+                                <select id='myselect form-control'>
+                                    <option>Option 1</option>
+                                    <option>Option 2</option>
+                                    <option>Option 3</option>
+
+
+                                </select>
                             </div>
                             <div class="form-group">
                                 <label for="recipient-name" class="col-form-label">Berat (kg)</label>
@@ -299,8 +306,11 @@
     </div>
 
     <!-- Bootstrap core JavaScript-->
+    <script src="../../js/demo/datatables-demo.js"></script>
+
     <script src="../../assets/vendor/jquery/jquery.min.js"></script>
     <script src="../../assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
 
     <!-- Core plugin JavaScript-->
     <script src="../../assets/vendor/jquery-easing/jquery.easing.min.js"></script>
@@ -313,7 +323,15 @@
     <script src="../../assets/vendor/datatables/dataTables.bootstrap4.min.js"></script>
 
     <!-- Page level custom scripts -->
-    <script src="../../js/demo/datatables-demo.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.10/js/select2.min.js"></script>
+    <script>
+        $('select').select2({
+            width: '100%',
+            placeholder: "Select an Option",
+            allowClear: true
+        });
+    </script>
 
 </body>
 
